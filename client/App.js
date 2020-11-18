e191d5492cc08bd45f503e80e3b2e8330821eb93
chas <hugginsc10@gmail.com>
2020-11-18 00:19:18 -0500

    homescreen styling

--- client/App.js
+++ client/App.js
@@ -14,6 +14,32 @@ export default function App() {
   const [loading, setLoading] = useState(true)
   const [user, setUser] = useState(null)
 
+  if (loading) {	
+    return (	
+      <></>	
+    )	
+  }
+
+  useEffect(() => {
+    const usersRef = firebase.firestore().collection('users');
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        usersRef
+          .doc(user.uid)
+          .get()
+          .then((document) => {
+            const userData = document.data()
+            setLoading(false)
+            setUser(userData)
+          })
+          .catch((error) => {
+            setLoading(false)
+          });
+      } else {
+        setLoading(false)
+      }
+    });
+  }, []);
   return (
     <NavigationContainer>
       <Stack.Navigator>

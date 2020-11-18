c0e57c08ce55c10c22558d03e233f6c36dd51fb0
chas <hugginsc10@gmail.com>
2020-11-18 00:17:11 -0500

    login screen and registration screen

---
+++ client/src/firebase/config.js
@@ -0,0 +1,21 @@
+
+import * as firebase from 'firebase';
+import '@firebase/auth';
+import '@firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyANfbdUCPJ4Ksg_0I1-X6uviVHfapyi4Aw",
+    authDomain: "budgetly-7d20e.firebaseapp.com",
+    databaseURL: "https://budgetly-7d20e.firebaseio.com",
+    projectId: "budgetly-7d20e",
+    storageBucket: "budgetly-7d20e.appspot.com",
+    messagingSenderId: "839435537712",
+    appId: "1:839435537712:web:427dcb6e8fdeed05ae4cf5",
+    measurementId: "G-FXMGVTKQD2"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };

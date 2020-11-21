1d03990452930cbcd314c234d682318cf1f56ed9
lizzie friedman <alizafriedman123@gmail.com>
2020-11-21 13:45:38 -0500

    expense path linked to firestore, add functionality

--- client/src/screens/index.js
+++ client/src/screens/index.js
@@ -1,5 +1,5 @@
 export { default as LoginScreen } from './LoginScreen/LoginScreen'
-export { default as Menu } from './Menu/Menu'
+export { default as MenuScreen } from './MenuScreen/MenuScreen'
 export { default as HomeScreen } from './HomeScreen/HomeScreen'
 export { default as RegistrationScreen } from './RegistrationScreen/RegistrationScreen'
 export {default as Dash} from './Dash/Dash'

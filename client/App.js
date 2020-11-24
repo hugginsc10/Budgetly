ed5770d5de4055c9d9606ffe392c363eb1321b23
lizzie friedman <alizafriedman123@gmail.com>
2020-11-23 20:56:09 -0500

    fix app

--- client/App.js
+++ client/App.js
@@ -46,18 +46,18 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         { user ? (
-          <Stack.Screen name="Expenses" >
-            {props => <Expenses {...props} extraData={user} />}
+          <Stack.Screen name="Home" >
+            {props => <HomeScreen {...props} extraData={user} />}
           </Stack.Screen>
         ) : (
           <>
             <Stack.Screen name="Login" component={LoginScreen} />
             <Stack.Screen name="Registration" component={RegistrationScreen} />
               <Stack.Screen name='Menu' component={MenuScreen} />
-              {/* <Stack.Screen name='Dash' component={Dash} /> */}
+              <Stack.Screen name='Dash' component={Dash} />
               <Stack.Screen name='Expenses' component={Expenses} />
-              {/* <Stack.Screen name='Income' component={Income} /> */}
-              {/* <Stack.Screen name='Goals' component={Goals} /> */}
+              <Stack.Screen name='Income' component={Income} />
+              <Stack.Screen name='Goals' component={Goals} />
 
           </>
         )}

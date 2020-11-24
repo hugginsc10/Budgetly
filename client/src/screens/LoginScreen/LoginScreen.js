8d173fdee28880cd816754bf2784b6bb5e0a6e44
lizzie friedman <alizafriedman123@gmail.com>
2020-11-23 22:44:42 -0500

    rewrite login?

--- client/src/screens/LoginScreen/LoginScreen.js
+++ client/src/screens/LoginScreen/LoginScreen.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react'
 import { Image, Text, TextInput, TouchableOpacity, View, Button } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import styles from './styles';
-import { firebase } from '../../firebase/config'
+import { firebase, db } from '../../firebase/config'
 import { SignInWithFacebook } from '../../firebase/auth'
 import { FacebookSignIn} from '../RegistrationScreen/RegistrationScreen'
+import { auth } from 'react-native-firebase';
 
 export default function LoginScreen({navigation}) {
     const [email, setEmail] = useState('')
@@ -14,32 +15,54 @@ export default function LoginScreen({navigation}) {
         navigation.navigate('Registration')
     }
    
-    const onLoginPress = () => {
-        firebase
-            .auth()
-            .signInWithEmailAndPassword(email, password)
-            .then((response) => {
-                const uid = response.user.uid
-                const usersRef = firebase.firestore().collection('users')
-                usersRef
-                    .doc(uid)
-                    .get()
-                    .then(firestoreDocument => {
-                        if (!firestoreDocument.exists) {
-                            alert("User does not exist anymore.")
-                            return;
-                        }
-                        const user = firestoreDocument.data()
-                        navigation.navigate('Home', {user})
-                    })
-                    .catch(error => {
-                        alert(error)
-                    });
-            })
-            .catch(error => {
-                alert(error)
-            })
+
+    const onLoginPress = async (email, password) => {
+        const res = await  firebase.auth().signInWithEmailAndPassword(email, password)
+        const uid = res.user.uid
+        const user = db.collection(`users/${uid}`).get()
+        // const user = firestoreDocument.data()
+        navigation.navigate('Home', {user})
+
+        
     }
+
+
+    // const cityRef = db.collection('cities').doc('SF');
+    // const doc = await cityRef.get();
+    // if (!doc.exists) {
+    //     console.log('No such document!');
+    // } else {
+    //     console.log('Document data:', doc.data());
+    // }
+
+
+
+    // const onLoginPress = () => {
+    //     firebase
+    //         .auth()
+    //         .signInWithEmailAndPassword(email, password)
+    //         .then((response) => {
+    //             const uid = response.user.uid
+    //             const usersRef = db.collection('users')
+    //             usersRef
+    //                 .doc(uid)
+    //                 .get()
+    //                 .then(firestoreDocument => {
+    //                     if (!firestoreDocument.exists) {
+    //                         alert("User does not exist anymore.")
+    //                         return;
+    //                     }
+    //                     const user = firestoreDocument.data()
+    //                     navigation.navigate('Home', {user})
+    //                 })
+    //                 .catch(error => {
+    //                     alert(error)
+    //                 });
+    //         })
+    //         .catch(error => {
+    //             alert(error)
+    //         })
+    // }
 
     return (
         <View style={styles.container}>
